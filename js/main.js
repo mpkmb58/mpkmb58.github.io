d7a631a7b132522d1c4687654572a9d8b5166bff
Ramadhan Agung Karyuto <rkaryuto@gmail,com>
2021-07-10 13:51:12 +0700

    memodifikasi landing page dan menambahkan beberapa halaman

---
+++ js/main.js
@@ -0,0 +1,17 @@
+import Swiper from 'swiper';
+var swiper = new Swiper(".mySwiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 0,
+          depth: 300,
+          modifier: 1,
+          slideShadows: true,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+        },
+});
