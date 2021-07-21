f7414fdc49bc0773b7f2d1d7a7a91cd2e5e5e58a
tioramadhn <tioramadhntio@apps.ipb.ac.id>
2021-07-22 02:24:18 +0700

    Membuat banner

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-
-let toggle = true;
-$('.menu').on('click', function(){
-    if(toggle){
-        $(this).addClass('open');
-        $('.overlay').removeClass('d-none');
-        gsap.from(".overlay", {duration: .3, ease: "power4", opacity: 0});
-        gsap.from(".link-side", {
-            duration: .8,
-            scale: 0.5, 
-            opacity: 0, 
-            delay: 0.5, 
-            stagger: 0.2,
-            ease: "elastic", 
-            force3D: true
-          });
-          
-        toggle = false;
-    }else{
-        $(this).removeClass('open');
-        $('.overlay').addClass('d-none');
-        toggle = true;
-      };
-
- });
-
-
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel();
+});
