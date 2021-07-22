73a6049cdd0645d021eb80ce2ebf3057262934b2
tioramadhn <tioramadhntio@apps.ipb.ac.id>
2021-07-22 19:55:30 +0700

    Fix size font in mobile and add animation while load content

--- js/main.js
+++ js/main.js
@@ -1,69 +1,71 @@
-$(document).ready(function () {
-
-  const test = () => $(".header").toggleClass('bg-grey');;
-  gsap.to(".header", {
-    scrollTrigger: {
-      trigger: ".b",
-      start: "top center",
-      scrub: true,
-      end: "bottom top",
-      toggleClass: { targets: ".header", className: "bg-grey" }
-    }, // start the animation when ".box" enters the viewport (once)
-    y: -20,
-  });
-
-  gsap.to(".coin", {
-    scrollTrigger: {
-      trigger: ".b",
-      scrub: 1,
-      start: "top bottom",
-      end: "top top"
-    },
-    x: -200,
-    ease: "none",
-    opacity: 0
-
-  });
-
-  gsap.to(".hero-mahardika", {
-    scrollTrigger: {
-      trigger: ".b",
-      scrub: 2,
-      start: "top bottom",
-      end: "top top"
-    },
-    y: 400,
-    ease: "none"
-  });
-
-  $('.menu').on('click', function () {
-    $(this).toggleClass('open');
-    $('.nav-collaps').toggleClass('show');
-    $('.wrapper').toggleClass('small');
-  });
+$(window).on("load", function () {
+  $(".loader-wrapper").fadeOut("slow");
+});
 
 
 
+const test = () => $(".header").toggleClass('bg-grey');;
+gsap.to(".header", {
+  scrollTrigger: {
+    trigger: ".b",
+    start: "top center",
+    scrub: true,
+    end: "bottom top",
+    toggleClass: { targets: ".header", className: "bg-grey" }
+  }, // start the animation when ".box" enters the viewport (once)
+  y: -20,
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+gsap.to(".coin", {
+  scrollTrigger: {
+    trigger: ".b",
+    scrub: 1,
+    start: "top bottom",
+    end: "top top"
+  },
+  x: -200,
+  ease: "none",
+  opacity: 0
 
 });
+
+gsap.to(".hero-mahardika", {
+  scrollTrigger: {
+    trigger: ".b",
+    scrub: 2,
+    start: "top bottom",
+    end: "top top"
+  },
+  y: 400,
+  ease: "none"
+});
+
+$('.menu').on('click', function () {
+  $(this).toggleClass('open');
+  $('.nav-collaps').toggleClass('show');
+  $('.wrapper').toggleClass('small');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
