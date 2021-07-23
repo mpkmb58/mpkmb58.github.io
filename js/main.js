5691b4934ec465f86939761af6e1e6c4b702093b
Ramadhan Agung Karyuto <rkaryuto@gmail,com>
2021-07-23 18:50:49 +0700

    memodifikasi halaman landing page

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
-var swiper = new Swiper(".mySwiper", {
-        effect: "coverflow",
-        grabCursor: true,
-        centeredSlides: true,
-        slidesPerView: "auto",
-        coverflowEffect: {
-          rotate: 0,
-          stretch: 0,
-          depth: 200,
-          modifier: 1,
-          slideShadows: true,
+$('.owl-carousel').owlCarousel({
+    margin:10,
+    responsive:{
+        0:{
+            items:1
         },
-        pagination: {
-          el: ".swiper-pagination",
+        540:{
+            items:2
         },
-      });
+        992: {
+          items: 3
+        },
+        1200: {
+          items: 4
+        },
+    }
+})
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
