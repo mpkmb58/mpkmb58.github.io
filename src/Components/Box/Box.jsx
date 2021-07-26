735dfcb271b0b45bb0b82c9657f13234c250bff3
Ramadhan Agung Karyuto <rkaryuto@gmail,com>
2021-07-26 16:48:51 +0700

    test commit

--- src/Components/Box/Box.jsx
+++ src/Components/Box/Box.jsx
@@ -1,21 +1,6 @@
 import React from 'react'
 import texture from '../../Assets/img/texture.svg'
 import './Box.css'
-// <div className="box bg-blue p-5">
-        //     <div className="texture-1">
-        //         <img src="img/texture.svg" alt="" srcset="">
-        //     </div>
-        //     <p className="texture-number text-grey">1</p>
-        //     <h5 className="text-center text-grey">Mentoring MPKMB</h5>
-        //     <p className="date text-grey mr-2">Senin, 9 Agustus 2021 <br><span class="fw-lighter">8.00 WIB - 17.00 WIB</span></p>
-        //     <div className="contentB">
-        //         <div className="content p-3">
-        //             <a href="" className="py-1 px-3 rounded-pill">
-        //                 Selengkapnya
-        //             </a>
-        //         </div>
-        //     </div>
-        // </div>
 
 const Box = (props) => {
     return (
