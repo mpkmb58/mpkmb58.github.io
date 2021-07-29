20920a298753a52f55fc11d186f4740f69dbadde
tioramadhn <tioramadhntio@apps.ipb.ac.id>
2021-07-29 09:29:23 +0700

    menghapus depedencies yg tidak dibutuhkan dan memperbaiki size texture dan animasi

--- src/Components/CardLiniMasa/CardLiniMasa.jsx
+++ src/Components/CardLiniMasa/CardLiniMasa.jsx
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from 'react'
 import $ from 'jquery';
 import Box from '../Box/Box'
 import OwlCarousel from 'react-owl-carousel';
@@ -11,21 +11,21 @@ function CardLiniMasa() {
     const options = {
         margin: 16,
         responsiveClass: true,
-        owldots : true,
+        owldots: true,
         nav: true,
         smartSpeed: 1000,
         responsive: {
-            0:{
-            items:1
+            0: {
+                items: 1
             },
-            540:{
-                items:2
+            540: {
+                items: 2
             },
             992: {
-            items: 3
+                items: 3
             },
             1200: {
-            items: 4
+                items: 4
             }
         },
     };
@@ -37,43 +37,44 @@ function CardLiniMasa() {
                 <h3 className="text-center text-blue fw-bold">LINI MASA</h3>
                 <OwlCarousel className="mt-3" {...options}>
                     <div className="item mx-3">
-                        <Box color="bg-blue" 
-                        number="1" 
-                        title="Mentoring MPKMB" 
-                        date ="23 Juli 2021" 
-                        time="08.00 WIB - 17.00 WIB"
-                        modalID ="mentoring"/>
+                        <Box color="bg-blue"
+                            number="1"
+                            title="Mentoring MPKMB"
+                            date="23 Juli 2021"
+                            time="08.00 WIB - 17.00 WIB"
+                            modalID="mentoring" />
                     </div>
                     <div className="item mx-3">
-                        <Box color="bg-red" 
-                        number="2" 
-                        title="Panduan MPKMB" 
-                        date ="29 Juli 2021" 
-                        time="08.00 WIB - 17.00 WIB"
-                        modalID="panduan"/>
+                        <Box color="bg-red"
+                            number="2"
+                            title="Panduan MPKMB"
+                            date="29 Juli 2021"
+                            time="08.00 WIB - 17.00 WIB"
+                            modalID="panduan" />
                     </div>
                     <div className="item mx-3">
                         <Box color="bg-orange"
-                         number="3" 
-                         title="Surat Keputusan Rektor" 
-                         date ="30 Juli 2021" 
-                         time="08.00 WIB - 17.00 WIB"
-                         modalID="sk"/>
-                    </div>
-                    <div className="item mx-3">
-                        <Box color="bg-blue" 
-                            number="4" 
-                            title="Upacara Pembukaan MPKMB" 
-                            date ="9 Agustus 2021" 
+                            number="3"
+                            title="Surat Keputusan Rektor"
+                            date="30 Juli 2021"
                             time="08.00 WIB - 17.00 WIB"
-                            modalID="opening"/>
+                            modalID="sk" />
                     </div>
                     <div className="item mx-3">
-                        <Box color="bg-red" 
-                        number="5" 
-                        title="Narasi Besar" 
-                        date ="9 Agustus 2021" 
-                        time="08.00 WIB - 17.00 WIB"/>
+                        <Box color="bg-blue"
+                            number="4"
+                            title="Upacara Pembukaan MPKMB"
+                            date="9 Agustus 2021"
+                            time="08.00 WIB - 17.00 WIB"
+                            modalID="opening" />
+                    </div>
+                    <div className="item mx-3">
+                        <Box color="bg-red"
+                            number="5"
+                            title="Narasi Besar"
+                            date="9 Agustus 2021"
+                            time="08.00 WIB - 17.00 WIB"
+                            modalID="narasi" />
                     </div>
                 </OwlCarousel>
             </div>
--- src/Components/SectionB/SectionB.jsx
+++ src/Components/SectionB/SectionB.jsx
@@ -18,26 +18,26 @@ import './SectionB.css'
 
 function SectionB() {
     const logoLogo = <div className="element-bg d-flex">
-        <img className="mx-3 bg-logo" src={logo1} alt="logo-bg" width="100%" />
-        <img className="mx-3 bg-logo" src={logo2} alt="logo-bg" width="100%" />
-        <img className="mx-3 bg-logo" src={logo3} alt="logo-bg" width="100%" />
-        <img className="mx-3 bg-logo" src={logo4} alt="logo-bg" width="100%" />
-        <img className="mx-3 bg-logo" src={logo5} alt="logo-bg" width="100%" />
+        <img className="mx-3 bg-logo" src={logo1} alt="logo-bg" />
+        <img className="mx-3 bg-logo" src={logo2} alt="logo-bg" />
+        <img className="mx-3 bg-logo" src={logo3} alt="logo-bg" />
+        <img className="mx-3 bg-logo" src={logo4} alt="logo-bg" />
+        <img className="mx-3 bg-logo" src={logo5} alt="logo-bg" />
     </div>
 
     const logoConsole = <div className="logo-console-wrapper element-bg">
-        <img className="bg-logo" src={logoconsole} alt="logo-bg" width="100%" />
+        <img className="bg-logo" src={logoconsole} alt="logo-bg" />
     </div>
 
-    const logoFAQ = <div className="element-bg"><img className="mx-3 bg-logo" src={logofaq} alt="logo-bg" width="100%" /> </div>
+    const logoFAQ = <div className="element-bg"><img className="mx-3 bg-logo" src={logofaq} alt="logo-bg" /> </div>
 
     const penugasan =
         <div className="wrapper position-absolute d-flex">
             <div className="scale-up agenda-logo-wrapper element-bg">
-                <img className="mx-3 bg-logo" src={logoAgenda} alt="logo-bg" width="100%" />
+                <img className="mx-3 bg-logo" src={logoAgenda} alt="logo-bg" />
             </div>
             <div className="scale-up penugasan-logo-wrapper element-bg">
-                <img className="mx-3 bg-logo" src={logoPenugasan} alt="logo-bg" width="100%" />
+                <img className="mx-3 bg-logo" src={logoPenugasan} alt="logo-bg" />
             </div>
         </div>
 
@@ -69,7 +69,7 @@ function SectionB() {
                 content=
                 {
                     <Fragment>
-                        <iframe src="https://drive.google.com/file/d/1nnoVjlCXwGVSYG39SJIOicxGOU3wjKub/preview" width="640" height="350" allow="autoplay"></iframe>
+                        <iframe className="iframe-size" src="https://drive.google.com/file/d/1nnoVjlCXwGVSYG39SJIOicxGOU3wjKub/preview" allow="autoplay"></iframe>
                         <button className="mt-3 bg-blue py-1 px-3 rounded-pill text-grey">Download PDF</button>
                     </Fragment>
                 }
@@ -82,7 +82,7 @@ function SectionB() {
                 content=
                 {
                     <Fragment>
-                        <iframe src="https://drive.google.com/file/d/1j7YX38amioVZ70wvJgo76BqY6jzXfGDV/preview" width="640" height="350" allow="autoplay"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1j7YX38amioVZ70wvJgo76BqY6jzXfGDV/preview" className="iframe-size" allow="autoplay"></iframe>
                         <button className="mt-3 bg-blue py-1 px-3 rounded-pill text-grey">Download PDF</button>
                     </Fragment>
                 }
@@ -92,6 +92,12 @@ function SectionB() {
                 id="opening"
                 title="Opening MPKMB"
                 content={<p className="text-center text-blue">Upacara pembukaan Masa Pengenalan Kampus Mahasiswa Baru atau <span className="fw-bold">Opening Ceremony MPKMB</span> merupakan momentum peresmian pembukaan rangkaian acara MPKMB yang akan dilaksanakan pada tanggal <span className="fw-bold">9 Agustus 2021.</span></p>}
+                size="modal-lg"
+            />
+            <Modal
+                id="narasi"
+                title="Coming Soon"
+                content={`Content Will be update soon!`}
                 size="modal-lg"
             />
             <section className="b py-4" >
@@ -106,7 +112,8 @@ function SectionB() {
                                 texture={true}
                                 bgPositionTop='texture-top-left'
                                 bgPositionBottom='texture-bottom-right'
-                                animateBottom={true} />
+                                animateBottom={true}
+                                animateTop={true} />
                         </div>
                     </div>
                     <div className="row mt-3">
@@ -125,7 +132,7 @@ function SectionB() {
                                 texture={true}
                                 bgPositionTop='texture-top-left'
                                 bgPositionBottom='texture-bottom-right'
-                            />
+                                animateTop={true} />
                         </div>
                         <div className="col-md-6 mt-3 mt-md-0">
                             <Card
